1fa811c9f0f6721cb0823b02105391e99be160fa
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 10:38:36 +0000

    use evaluator.js

--- src/math/calculate.tsx
+++ src/math/calculate.tsx
@@ -4,31 +4,37 @@ import {
 	useString,
 	createElement,
 	Message,
+	Embed,
 } from "slshx";
-import exactMath from "exact-math";
-
-const error = () => (
-	<Message ephemeral>Failed to calculate expression!</Message>
-);
+import evaluate from "evaluator.js";
+import { red } from "../design-system/colors";
 
 export function calculate(): CommandHandler<Env> {
 	useDescription("Calculates an expression");
 	const expression = useString("expression", "The maths expression.", {
 		required: true,
 	});
-	try {
-		setTimeout(function () {
-			return error;
-		}, 5000);
-		const expressionResult = exactMath.formula(expression.replace(" ", ""));
-		return () => (
-			<Message>
-				```
-				{expression} = {expressionResult}
-				```
-			</Message>
-		);
-	} catch {
-		return error;
-	}
+	return async () => {
+		try {
+			const expressionResult = evaluate(expression.replace(" ", ""));
+			return (
+				<Message>
+					```
+					{expression} = {expressionResult}
+					```
+				</Message>
+			);
+		} catch (err) {
+			return (
+				<Message ephemeral>
+					<Embed
+						title="Failed to calculate expression."
+						color={red()}
+					>
+						{err}
+					</Embed>
+				</Message>
+			);
+		}
+	};
 }
