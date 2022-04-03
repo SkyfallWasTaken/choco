85caaa0c691db6179c766c0f177b0b4787d1aa93
Skyfall <mahadkalam1234@gmail.com>
2022-04-03 10:49:02 +0100

    Make code better, add emojis

--- src/games/rockpaperscissors.tsx
+++ src/games/rockpaperscissors.tsx
@@ -8,46 +8,50 @@ import {
 	useDescription,
 	Row,
 } from "slshx";
-import { random } from "lodash-es";
+import { sample } from "lodash-es";
 import { blue } from "../design-system/colors";
 
+/* eslint-disable */
 enum Choice {
-	Rock,
-	Paper,
-	Scissors,
+	Rock = "✊ Rock",
+	Paper = "✋ Paper",
+	Scissors = "✂️ Scissors",
 }
+/* eslint-enable */
 
 function determineWinner(userChoice: Choice, botChoice: Choice): string {
 	if (userChoice === botChoice) return "it's a draw!";
 
 	switch (botChoice) {
-	case Choice.Rock: {
-		if (userChoice === Choice.Paper) return "you win!";
-		return "I win!";
-	}
-	case Choice.Paper: {
-		if (userChoice === Choice.Scissors) return "you win!";
-		return "I win!";
-	}
-	case Choice.Scissors: {
-		if (userChoice === Choice.Rock) return "you win!";
-		return "I win!";
-	}
+		case Choice.Rock: {
+			if (userChoice === Choice.Paper) return "you win!";
+			return "I win!";
+		}
+		case Choice.Paper: {
+			if (userChoice === Choice.Scissors) return "you win!";
+			return "I win!";
+		}
+		case Choice.Scissors: {
+			if (userChoice === Choice.Rock) return "you win!";
+			return "I win!";
+		}
 	}
 }
 
 function tttChoice(choice: Choice) {
-	const r = random(1, 3);
-	const botChoice =
-		r === 1 ? Choice.Rock : r === 2 ? Choice.Paper : Choice.Scissors;
+	const botChoice = sample([
+		Choice.Rock,
+		Choice.Paper,
+		Choice.Scissors,
+	]) as Choice;
 
 	return (
 		<Message update>
 			<Embed
-				title={`Rock Paper Scissors: you chose ${choice}`}
+				title={`Rock Paper Scissors: you chose ${choice.toString()}`}
 				color={blue()}
 			>
-				My choice is **{botChoice}**,{" "}
+				My choice is **{botChoice.toString()}**,{" "}
 				{determineWinner(choice, botChoice)}
 			</Embed>
 		</Message>
@@ -63,17 +67,17 @@ export function rockpaperscissors(): CommandHandler<Env> {
 	return () => (
 		<Message>
 			<Embed title="Rock Paper Scissors" color={blue()}>
-				Choose rock, paper or scissors!
+				Choose **{Choice.Rock}, {Choice.Paper} or {Choice.Scissors}!**
 			</Embed>
 			<Row>
 				<Button id={rock} primary>
-					Rock
+					{Choice.Rock}
 				</Button>
 				<Button id={paper} primary>
-					Paper
+					{Choice.Paper}
 				</Button>
 				<Button id={scissors} primary>
-					Scissors
+					{Choice.Scissors}
 				</Button>
 			</Row>
 		</Message>
