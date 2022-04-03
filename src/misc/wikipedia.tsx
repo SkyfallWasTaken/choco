c180667b2cb7a40e60fc80204b0fa25f567ae882
Skyfall <mahadkalam1234@gmail.com>
2022-04-03 10:24:52 +0100

    Use discord's autocomplete for a better experience

--- src/misc/wikipedia.tsx
+++ src/misc/wikipedia.tsx
@@ -9,6 +9,7 @@ import {
 } from "slshx";
 import fetchWikipedia from "../util/wikipediaRequest";
 import { blue } from "../design-system/colors";
+import Error from "../components/Error";
 
 type PageSummary = {
 	type: string;
@@ -24,7 +25,8 @@ type PageSummary = {
 		source: string;
 	};
 };
-type Search = [string, string];
+
+type Search = [string, string[]];
 
 async function DefinitionEmbed({
 	displaytitle: displayTitle,
@@ -54,7 +56,7 @@ export function wikipediaRandom(): CommandHandler<Env> {
 
 	return async function* () {
 		yield;
-		
+
 		const page: PageSummary = await (
 			await fetchWikipedia("/page/random/summary")
 		).json();
@@ -64,30 +66,37 @@ export function wikipediaRandom(): CommandHandler<Env> {
 
 export function wikipediaLookup(): CommandHandler<Env> {
 	useDescription("Lookup a page on Wikipedia.");
-	const pageName = useString("page", "The page to look up.", {
+	const pageName = useString<Env>("page", "The page to look up.", {
 		required: true,
+		async autocomplete() {
+			console.log(1);
+			const searchParams = new URLSearchParams({
+				action: "opensearch",
+				format: "json",
+				formatversion: "2",
+				search: pageName,
+				namespace: "0",
+				limit: "5",
+			});
+			const search = await fetchWikipedia(`?${searchParams}`, true);
+			const searchResult: string[] = ((await search.json()) as Search)[1];
+
+			return searchResult;
+		},
 	});
 
 	return async function* () {
 		yield;
-		const searchParams = new URLSearchParams({
-			action: "opensearch",
-			format: "json",
-			formatversion: "2",
-			search: pageName,
-			namespace: "0",
-			limit: "2",
-		});
-		const search = await fetchWikipedia(`?${searchParams}`, true);
-		const searchResult = ((await search.json()) as Search)[1];
-		if (searchResult.length === 0) {
-			return <Message>The page `{pageName}` could not be found.</Message>;
-		}
-		const fuzzyPageName = searchResult[0];
 		const page = await fetchWikipedia(
-			`/page/summary/${encodeURIComponent(fuzzyPageName)}?redirect=false`
+			`/page/summary/${encodeURIComponent(pageName)}?redirect=false`
 		);
 		const pageData: PageSummary = await page.json();
+		if (page.status === 404)
+			return (
+				<Message>
+					<Error error={`Page ${pageName} not found.`}></Error>
+				</Message>
+			);
 		if (pageData.type != "standard") {
 			return (
 				<Message>
